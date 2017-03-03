6868bd99f052e453d26ee8c3b7f42df39be78e62
Bodo Tasche <bodo@wannawork.de>
2017-03-03 18:24:12 +0100

    added credo, fixes #8

--- mix.exs
+++ mix.exs
@@ -37,7 +37,9 @@ defmodule SignDict.Mixfile do
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:credo, "~> 0.5", only: [:dev, :test]}
+   ]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
