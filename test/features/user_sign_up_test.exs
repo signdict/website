129e1e5598920bb80188471b06cbb3e457575db7
Bodo Tasche <bodo@wannawork.de>
2017-03-07 09:51:09 +0100

    completed wallaby integration test

--- test/features/user_sign_up_test.exs
+++ test/features/user_sign_up_test.exs
@@ -1,18 +1,41 @@
 defmodule SignDict.UserSignUpTest do
-  use SignUp.AcceptanceCase, async: true
+  use SignUp.AcceptanceCase
 
-  test "users have names", %{session: session} do
-    first_employee =
-      session
-      |> visit("/")
-      # |> find(Query.css(".dashboard"))
-      # |> all(Query.css(".user"))
-      # |> List.first
-      # |> find(Query.css(".user-name"))
-      |> text
+  test "sign up user, login and logout", %{session: session} do
+    registration = session
+                   |> visit("/")
+                   |> click(Query.link("Register"))
+                   |> find(Query.css(".user-form"), fn(form) ->
+                     form
+                     |> fill_in(Query.text_field("user_email"), with: "elisa@example.com")
+                     |> fill_in(Query.text_field("user_password"), with: "mylongpassword")
+                     |> fill_in(Query.text_field("user_password_confirmation"), with: "mylongpassword")
+                     |> fill_in(Query.text_field("user_name"), with: "Elisa Example")
+                     |> click(Query.button("Register"))
+                   end)
+                   |> find(Query.css(".alert-info"))
+                   |> Element.text
+    assert registration == "Registration successful"
 
-    # TODO: finish this
+    log_in = session
+             |> visit("/")
+             |> click(Query.link("Sign in"))
+             |> find(Query.css(".login-form"), fn(form) ->
+               form
+               |> fill_in(Query.text_field("session_email"), with: "elisa@example.com")
+               |> fill_in(Query.text_field("session_password"), with: "mylongpassword")
+               |> click(Query.button("Log in"))
+             end)
+             |> find(Query.css(".alert-info"))
+             |> Element.text
+    assert log_in == "Successfully signed in"
 
-    # assert first_employee == "Chris"
+    log_out = session
+              |> visit("/")
+              |> click(Query.link("Sign out"))
+              |> find(Query.css(".alert-info"))
+              |> Element.text
+
+    assert log_out == "Successfully signed out"
   end
 end
