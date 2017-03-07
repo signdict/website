e72b394cb20a2f4ab4bfa895b9036048107cde3d
Bodo Tasche <bodo@wannawork.de>
2017-03-07 10:24:42 +0100

    refactored feature test

--- test/features/user_sign_up_test.exs
+++ test/features/user_sign_up_test.exs
@@ -2,40 +2,37 @@ defmodule SignDict.UserSignUpTest do
   use SignUp.AcceptanceCase, async: true
 
   test "sign up user, login and logout", %{session: session} do
-    registration = session
-                   |> visit("/")
-                   |> click(Query.link("Register"))
-                   |> find(Query.css(".user-form"), fn(form) ->
-                     form
-                     |> fill_in(Query.text_field("user_email"), with: "elisa@example.com")
-                     |> fill_in(Query.text_field("user_password"), with: "mylongpassword")
-                     |> fill_in(Query.text_field("user_password_confirmation"), with: "mylongpassword")
-                     |> fill_in(Query.text_field("user_name"), with: "Elisa Example")
-                     |> click(Query.button("Register"))
-                   end)
-                   |> find(Query.css(".alert-info"))
-                   |> Element.text
-    assert registration == "Registration successful"
+    session
+    |> visit("/")
+    |> click(Query.link("Register"))
+    |> find(Query.css(".user-form"), fn(form) ->
+      form
+      |> fill_in(Query.text_field("user_email"), with: "elisa@example.com")
+      |> fill_in(Query.text_field("user_password"), with: "mylongpassword")
+      |> fill_in(Query.text_field("user_password_confirmation"), with: "mylongpassword")
+      |> fill_in(Query.text_field("user_name"), with: "Elisa Example")
+      |> click(Query.button("Register"))
+    end)
+    |> assert_alert("Registration successful")
+    |> visit("/")
+    |> click(Query.link("Sign in"))
+    |> find(Query.css(".login-form"), fn(form) ->
+      form
+      |> fill_in(Query.text_field("session_email"), with: "elisa@example.com")
+      |> fill_in(Query.text_field("session_password"), with: "mylongpassword")
+      |> click(Query.button("Log in"))
+    end)
+    |> assert_alert("Successfully signed in")
+    |> visit("/")
+    |> click(Query.link("Sign out"))
+    |> assert_alert("Successfully signed out")
+  end
 
-    log_in = session
-             |> visit("/")
-             |> click(Query.link("Sign in"))
-             |> find(Query.css(".login-form"), fn(form) ->
-               form
-               |> fill_in(Query.text_field("session_email"), with: "elisa@example.com")
-               |> fill_in(Query.text_field("session_password"), with: "mylongpassword")
-               |> click(Query.button("Log in"))
-             end)
-             |> find(Query.css(".alert-info"))
-             |> Element.text
-    assert log_in == "Successfully signed in"
-
-    log_out = session
-              |> visit("/")
-              |> click(Query.link("Sign out"))
-              |> find(Query.css(".alert-info"))
-              |> Element.text
-
-    assert log_out == "Successfully signed out"
+  defp assert_alert(page, text) do
+    page
+    |> find(Query.css(".alert-info"), fn(notification) ->
+      assert notification
+      |> has_text?(text)
+    end)
   end
 end
