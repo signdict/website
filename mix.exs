7bd88e69c387b5bfee96729df92904c7ca114e79
Bodo Tasche <bitboxer@users.noreply.github.com>
2017-03-09 12:02:27 +0100

    Update plug (#33)
    There was a security issue with plug,
    updated it to latest version.
    
    For details see
    [here](https://elixirforum.com/t/security-releases-for-plug/3913)

--- mix.exs
+++ mix.exs
@@ -35,6 +35,7 @@ defmodule SignDict.Mixfile do
     [{:phoenix, "~> 1.2.1"},
      {:phoenix_pubsub, "~> 1.0"},
      {:phoenix_ecto, "~> 3.0"},
+     {:plug, "~> 1.3.3"},
      {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
