07c8a93ea23f1198e10a359019e5be86dc83d81a
Bodo Tasche <bitboxer@users.noreply.github.com>
2017-03-10 19:01:17 +0100

    Add password reset (#39)
    * add password reset functionality
    
    * emailer
    
    * added all translations
    
    * added subject to translations
    
    * tests for new changesets in user
    
    * tests for password reset controller
    
    * finished controller test
    
    * finished integration test for password change

---
+++ lib/sign_dict/mailer.ex
@@ -0,0 +1,3 @@
+defmodule SignDict.Mailer do
+  use Bamboo.Mailer, otp_app: :sign_dict
+end
--- web/models/user.ex
+++ web/models/user.ex
@@ -2,6 +2,7 @@ defmodule SignDict.User do
   use SignDict.Web, :model
 
   alias Comeonin.Bcrypt
+  alias Ecto.Changeset
 
   schema "users" do
     field :email, :string
@@ -15,12 +16,12 @@ defmodule SignDict.User do
 
     field :role, :string
 
+    field :password_reset_token, :string
+    field :password_reset_unencrypted, :string, virtual: true
+
     timestamps()
   end
 
-  @doc """
-  Builds a changeset based on the `struct` and `params`.
-  """
   def register_changeset(struct, params \\ %{}) do
     struct
     |> cast(params, [:email, :password, :password_confirmation,
@@ -31,6 +32,41 @@ defmodule SignDict.User do
     |> validate_confirmation(:password)
     |> hash_password()
     |> unique_constraint(:email)
+  end
+
+  def create_reset_password_changeset(struct) do
+    unencrypted_token = SecureRandom.urlsafe_base64(32)
+    encrypted_token = Bcrypt.hashpwsalt(unencrypted_token)
+
+    struct
+    |> change
+    |> put_change(:password_reset_unencrypted, unencrypted_token)
+    |> put_change(:password_reset_token, encrypted_token)
+  end
+
+  def reset_password_changeset(struct, params) do
+    struct
+    |> cast(params, [:password, :password_confirmation,
+                     :password_reset_unencrypted])
+    |> validate_required([:password, :password_confirmation])
+    |> validate_token
+    |> validate_confirmation(:password)
+    |> hash_password
+  end
+
+  defp validate_token(%{valid?: false} = changeset), do: changeset
+  defp validate_token(%{valid?: true} = changeset) do
+    {:ok, reset_unencrypted} =
+      Changeset.fetch_change(changeset, :password_reset_unencrypted)
+    {_, reset_encrypted}     =
+      Changeset.fetch_field(changeset, :password_reset_token)
+    token_matches = Bcrypt.checkpw(reset_unencrypted, reset_encrypted)
+    do_validate_token(token_matches, changeset)
+  end
+
+  defp do_validate_token(true, changeset), do: changeset
+  defp do_validate_token(false, changeset) do
+    Changeset.add_error changeset, :password_reset_token, "invalid"
   end
 
   defp hash_password(%{valid?: false} = changeset), do: changeset
