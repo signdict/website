8fdf8e05a5e0cac91eef3126ef77147a8dea7df4
Bodo Tasche <bodo@wannawork.de>
2017-03-29 07:20:15 +0200

    fixed upload path on production

--- lib/sign_dict/endpoint.ex
+++ lib/sign_dict/endpoint.ex
@@ -17,7 +17,7 @@ defmodule SignDict.Endpoint do
     only: ~w(css fonts images js favicon.ico robots.txt)
 
   plug Plug.Static,
-    at: "/uploads", from: Path.expand('./uploads'), gzip: false
+    at: "/uploads", from: Path.expand(Application.get_env(:sign_dict, :upload_path)), gzip: false
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
