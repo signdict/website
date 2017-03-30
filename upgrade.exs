25f2cf111b15d13077d86e31eb7b64d788a0317a
Bodo Tasche <bodo@wannawork.de>
2017-03-30 13:40:11 +0200

    deploy with yarn

--- upgrade.exs
+++ upgrade.exs
@@ -2,7 +2,7 @@ defmodule SignDict.UpgradeCallbacks do
   import Gatling.Bash
 
   def before_mix_digest(env) do
-    bash("npm", ~w[install], cd: env.build_dir)
+    bash("yarn", ~w[], cd: env.build_dir)
     bash("npm", ~w[run deploy], cd: env.build_dir)
   end
 
