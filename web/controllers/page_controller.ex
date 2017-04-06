48f38bf24e5bbf41e0dbadaf02aac4179a90f3e1
Bodo Tasche <bodo@wannawork.de>
2017-04-06 14:03:36 +0200

    added first version of landing page design

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,17 @@ defmodule SignDict.PageController do
   use SignDict.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    human_count = Repo.aggregate(SignDict.User, :count, :id)
+    sign_count  = Repo.aggregate(SignDict.Video, :count, :id)
+    render conn, "index.html", layout: {SignDict.LayoutView, "empty.html"},
+      human_count: human_count, sign_count: sign_count
+  end
+
+  def imprint(conn, _params) do
+    render conn, "imprint.html", layout: {SignDict.LayoutView, "empty.html"}
+  end
+
+  def contact(conn, _params) do
+    render conn, "contact.html", layout: {SignDict.LayoutView, "empty.html"}
   end
 end
