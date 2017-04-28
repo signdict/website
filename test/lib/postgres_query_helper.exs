8b5085453a1b566ad5a031739bf932742af4e72a
Bodo Tasche <bitboxer@users.noreply.github.com>
2017-04-28 15:38:00 +0200

    Delete empty words for search. Closes #93 (#94)

--- test/lib/postgres_query_helper.exs
+++ test/lib/postgres_query_helper.exs
@@ -7,5 +7,9 @@ defmodule SignDict.PostgresQueryHelperTest do
     test "it splits strings and adds :* to each one" do
       assert PostgresQueryHelper.format_search_query("Homer Simpson") == "Homer:*&Simpson:*"
     end
+
+    test "it trims the spaces away" do
+      assert PostgresQueryHelper.format_search_query("Homer  Simpson ") == "Homer:*&Simpson:*"
+    end
   end
 end
