84520e45b3da87989693fc6957375c6cc1e57247
Bodo Tasche <mail@signdict.org>
2017-05-07 14:35:08 +0200

    Redirect if entry does not have any videos to show, fixes #107

--- web/controllers/entry_controller.ex
+++ web/controllers/entry_controller.ex
@@ -8,15 +8,46 @@ defmodule SignDict.EntryController do
   alias SignDict.Video
 
   def show(conn, %{"id" => id}) do
-    entry  = Entry |> Entry.with_language |> Repo.get!(id)
-    videos = load_videos(entry)
-    voted  = Entry.voted_video(entry, conn.assigns.current_user)
-    video  = if voted && voted.id do
-      voted |> Video.with_vote_count
-    else
-      List.first(videos)
-    end
+    [entry, videos] = load_videos_for_entry(id)
 
+    if length(videos) > 0 do
+      voted  = Entry.voted_video(entry, conn.assigns.current_user)
+      video  = if voted && voted.id do
+        voted |> Video.with_vote_count
+      else
+        List.first(videos)
+      end
+
+      render_entry(conn, entry, videos, video, voted)
+    else
+      redirect_no_videos(conn)
+    end
+  end
+
+  def show(conn, %{"entry_id" => id, "video_id" => video_id}) do
+    [entry, videos] = load_videos_for_entry(id)
+
+    if length(videos) > 0 do
+      video  = Video
+               |> Repo.get!(video_id)
+               |> Repo.preload(:user)
+               |> Video.with_vote_count
+      voted = Entry.voted_video(entry, conn.assigns.current_user)
+
+      render_entry(conn, entry, videos, video, voted)
+    else
+      redirect_no_videos(conn)
+    end
+  end
+
+  defp load_videos_for_entry(entry_id) do
+    entry       = Entry |> Entry.with_language |> Repo.get!(entry_id)
+    video_query = Video.ordered_by_vote_for_entry(entry)
+    videos      = video_query |> Repo.all |> Repo.preload(:user)
+    [entry, videos]
+  end
+
+  defp render_entry(conn, entry, videos, video, voted) do
     render(conn, "show.html",
            layout: {SignDict.LayoutView, "empty.html"},
            entry: entry,
@@ -28,30 +59,10 @@ defmodule SignDict.EntryController do
          )
   end
 
-  def show(conn, %{"entry_id" => id, "video_id" => video_id}) do
-    entry  = Entry |> Entry.with_language |> Repo.get!(id)
-    videos = load_videos(entry)
-    video  = Video
-             |> Repo.get!(video_id)
-             |> Repo.preload(:user)
-             |> Video.with_vote_count
-
-    render(conn, "show.html",
-           layout: {SignDict.LayoutView, "empty.html"},
-           entry: entry,
-           video: video,
-           videos: videos,
-           voted_video: Entry.voted_video(entry, conn.assigns.current_user),
-           searchbar: true,
-           title: gettext("Sign for %{sign}", sign: entry.text)
-         )
-  end
-
-  defp load_videos(entry) do
-    video_query = Video.ordered_by_vote_for_entry(entry)
-    video_query
-    |> Repo.all
-    |> Repo.preload(:user)
+  defp redirect_no_videos(conn) do
+    conn
+    |> put_flash(:info, gettext("No videos found."))
+    |> redirect(to: "/")
   end
 
 end
