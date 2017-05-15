c1b8e8a35a6efd129bb7122ff3bc8299ba4f6010
Bodo Tasche <mail@signdict.org>
2017-05-15 10:03:32 +0200

    add opengraph renderer

--- web/views/shared_view.ex
+++ web/views/shared_view.ex
@@ -1,3 +1,14 @@
 defmodule SignDict.SharedView do
   use SignDict.Web, :view
+
+  def ogtags(assigns) do
+    if assigns[:ogtags] do
+      for {key, value} <- assigns[:ogtags] do
+        raw("\t<meta property=\"#{key}\" content=\"#{value}\">\n")
+      end
+    else
+      raw("\t<meta property=\"fb:app_id\" content=\"430839153628230\">\n")
+    end
+  end
+
 end
