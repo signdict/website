cbd1ee893e8e99cfeb7a60770cc8256ccba9ce56
Bodo Tasche <bitboxer@users.noreply.github.com>
2017-05-17 14:13:04 +0200

    Add twitter card (#121)
    * fixed translation for og embed
    
    * removed newlines in description
    
    * add twittercard, closes #84

--- test/lib/services/opengraph_test.exs
+++ test/lib/services/opengraph_test.exs
@@ -6,9 +6,13 @@ defmodule SignDict.Services.OpenGraphTest do
   test "it generates metadata for users" do
     user = insert(:user)
     result = SignDict.Services.OpenGraph.to_metadata(user)
-    assert result["og:description"] == "   Jane Smith is a user on SignDict, a sign language dictionary.\n"
-    assert result["og:image"] =~ "https://secure.gravatar.com/avatar/"
+    assert result["og:description"]      == "Jane Smith is a user on SignDict, a sign language dictionary."
+    assert result["og:image"]            =~ "https://secure.gravatar.com/avatar/"
     assert result["og:image:secure_url"] =~ "https://secure.gravatar.com/avatar/"
+    assert result["twitter:card"]        == "summary"
+    assert result["twitter:description"] == "Jane Smith is a user on SignDict, a sign language dictionary."
+    assert result["twitter:image"]       =~ "https://secure.gravatar.com/avatar/"
+    assert result["twitter:site"]        == "@SignDict"
   end
 
   test "it generates metadata for videos with a copyright line" do
@@ -16,16 +20,23 @@ defmodule SignDict.Services.OpenGraphTest do
     result = SignDict.Services.OpenGraph.to_metadata(video.entry, video)
     assert %{
       "og:description" => "This video shows the sign of \"some content\". " <>
-                          "See more Signs on SignDict.org,\nyour sign " <>
-                          "language dictionary.\nLicense: license " <>
-                          "by copyright - Jane Smith\n",
+                          "See more Signs on SignDict.org, your sign " <>
+                          "language dictionary. License: license " <>
+                          "by copyright - Jane Smith",
       "og:image" => "http://example.com/video.jpg",
       "og:image:secure_url" => "https://example.com/video.jpg",
       "og:type" => "video.other", "og:video:height" => 720,
       "og:video:secure_url" => "http://example.com/video.mp4",
       "og:video:type" => "video/mp4",
       "og:video:url" => "http://example.com/video.mp4",
-      "og:video:width" => 1280
+      "og:video:width" => 1280,
+      "twitter:card" => "player",
+      "twitter:description" => "This video shows the sign of \"some content\". See more Signs on SignDict.org, your sign language dictionary. License: license by copyright - Jane Smith",
+      "twitter:height" => 360,
+      "twitter:image" => "http://example.com/video.jpg",
+      "twitter:player" => "http://localhost:4001/embed/#{video.entry.id}-some-content/video/#{video.id}",
+      "twitter:site" => "@SignDict",
+      "twitter:width" => 640
     } == result
   end
 
@@ -34,16 +45,23 @@ defmodule SignDict.Services.OpenGraphTest do
     result = SignDict.Services.OpenGraph.to_metadata(video.entry, video)
     assert result == %{
       "og:description" => "This video shows the sign of \"some content\". " <>
-                          "See more Signs on SignDict.org,\nyour sign " <>
-                          "language dictionary.\nLicense: license " <>
-                          "by Jane Smith\n",
+                          "See more Signs on SignDict.org, your sign " <>
+                          "language dictionary. License: license " <>
+                          "by Jane Smith",
       "og:image" => "http://example.com/video.jpg",
       "og:image:secure_url" => "https://example.com/video.jpg",
       "og:type" => "video.other", "og:video:height" => 720,
       "og:video:secure_url" => "http://example.com/video.mp4",
       "og:video:type" => "video/mp4",
       "og:video:url" => "http://example.com/video.mp4",
-      "og:video:width" => 1280
+      "og:video:width" => 1280,
+      "twitter:card" => "player",
+      "twitter:description" => "This video shows the sign of \"some content\". See more Signs on SignDict.org, your sign language dictionary. License: license by Jane Smith",
+      "twitter:height" => 360,
+      "twitter:image" => "http://example.com/video.jpg",
+      "twitter:player" => "http://localhost:4001/embed/#{video.entry.id}-some-content/video/#{video.id}",
+      "twitter:site" => "@SignDict",
+      "twitter:width" => 640
     }
   end
 end
