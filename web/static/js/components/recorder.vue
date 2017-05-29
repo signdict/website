89b6857d61c78050a7ad5b61a9072f23b3d0e723
Bodo Tasche <mail@signdict.org>
2017-05-29 14:39:51 +0200

    added video recorder functionality

---
+++ web/static/js/components/recorder.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="recorder">
+    <video class="recorder--video recorder--video_flip" autoplay muted></video>
+    <div class="recorder--navbar">
+      <button v-on:click="startRecording">Start Recording!</button>
+      <button v-on:click="stopRecording">Stop Recording!</button>
+    </div>
+  </div>
+</template>
+
+<script>
+var streamHandle;
+var mediaRecorder;
+var recordedBlobs;
+
+function handleSuccess(stream) {
+  var previewVideo = document.getElementsByClassName("recorder--video")[0];
+  streamHandle = stream;
+  if (window.URL) {
+    previewVideo.src = window.URL.createObjectURL(stream);
+  } else {
+    previewVideo.src = stream;
+  }
+}
+
+function handleError(error) {
+  console.log("nope, not working!");
+  console.log(error);
+}
+
+function initRecorder() {
+  navigator.mediaDevices.getUserMedia({audio: false, video: true}).
+    then(handleSuccess).catch(handleError);
+}
+
+function handleStop(event) {
+  console.log('Recorder stopped: ', event);
+}
+
+function handleDataAvailable(event) {
+  console.log("getting data!");
+  if (event.data && event.data.size > 0) {
+    recordedBlobs.push(event.data);
+  }
+}
+
+function startRecording() {
+  recordedBlobs = []
+  try {
+    mediaRecorder = new MediaRecorder(streamHandle, {mimeType: "video/webm"});
+  } catch (e) {
+    console.error('Exception while creating MediaRecorder: ' + e);
+    alert('Exception while creating MediaRecorder: '
+      + e + '. mimeType: ' + options.mimeType);
+    return;
+  }
+  mediaRecorder.onstop = handleStop;
+  mediaRecorder.ondataavailable = handleDataAvailable;
+  mediaRecorder.start(100);
+  console.log("Media recording started!");
+}
+
+function stopRecording() {
+  mediaRecorder.stop();
+  console.log("Media recording stopped!");
+}
+
+export default {
+  created: function() {
+    console.log("calling method!");
+    initRecorder();
+    console.log("done!");
+  },
+
+  methods: {
+    startRecording: function(event) {
+      startRecording();
+    },
+
+    stopRecording: function(event) {
+      stopRecording();
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+html, body {
+  height: 100%;
+}
+
+.recorder {
+  width: 100%;
+  height: 100%;
+  padding-bottom: 5em;
+  background-color: #000;
+  overflow: hidden;
+}
+
+.recorder--video {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 100%;
+  height: 100%;
+}
+
+.recorder--video_flip {
+  -moz-transform: scale(-1, 1) !important;
+  -webkit-transform: scale(-1, 1) !important;
+  -o-transform: scale(-1, 1) !important;
+  transform: scale(-1, 1) !important;
+}
+</style>
--- web/static/js/recorder.js
+++ web/static/js/recorder.js
@@ -1,35 +1,18 @@
 import Vue from 'vue'
-import MyApp from "./components/my-app.vue"
+import Recorder from "./components/recorder.vue"
 
 document.onreadystatechange = function () {
   if (document.readyState == "complete") {
-    // Import "globally" used libraries -- importing them from Vue components alone
-    // seems to trip up brunch at the moment (they won't be included in the final
-    // app.js file)
-
-    // import "lodash"
-
-    // Create the main component
-
-    Vue.component('my-app', MyApp)
-
-    // And create the top-level view model:
+    Vue.component('recorder', Recorder)
 
     new Vue({
       el: '#app',
       data() {
         return {
-          // state for the top level component, e.g
-          currentMessage: "Hello World"
         }
       },
       render: function (createElement) {
-        return createElement(MyApp, {
-          props: {
-            // props for the top level component, e.g.
-            message: this.currentMessage
-          }
-        })
+        return createElement(Recorder, {})
       }
     });
   }
