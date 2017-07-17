97b4bbde98ab1ee340362c69b0ab9cc61dca0706
Bodo Tasche <mail@signdict.org>
2017-07-17 10:08:13 +0200

    Add plyr video player

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -2,3 +2,13 @@ import "phoenix_html"
 
 import Turbolinks from "turbolinks"
 Turbolinks.start();
+
+import plyr from "plyr"
+document.addEventListener("turbolinks:load", function() {
+  plyr.setup(document.getElementsByTagName('video'), {
+    controls: [
+      'play-large', 'play', 'progress', 'current-time', 'fullscreen'
+    ]
+  });
+});
+
