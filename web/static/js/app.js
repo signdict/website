138bc4d9ebcdcba4fafd30d69c8cbc6b5e604a7c
Bodo Tasche <bitboxer@users.noreply.github.com>
2017-07-25 11:18:53 +0200

    Add space to play/pause the video (#217)

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -3,14 +3,8 @@ import "phoenix_html"
 import Turbolinks from "turbolinks"
 Turbolinks.start();
 
-import plyr from "plyr"
-document.addEventListener("turbolinks:load", function() {
-  plyr.setup(document.getElementsByTagName('video'), {
-    controls: [
-      'play-large', 'play', 'progress', 'current-time', 'fullscreen'
-    ]
-  });
-});
-
 import browser from 'detect-browser';
 window.browser = browser;
+
+import Player from "./classes/player.js";
+new Player().init();
