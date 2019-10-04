b383af0eeb0797fb58100e0363149611de020c41
gulp21 <support.gulp21@googlemail.com>
2019-10-04 21:35:08 +0200

    Add Dockerfile and docker-compose (#557)

---
+++ docker-entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+cd /signdict
+
+echo "============" deps
+mix local.hex --force && mix local.rebar --force
+mix deps.get
+
+
+echo "============" setup
+mix ecto.setup
+
+echo "============" yarn
+cd assets
+yarn install
+cd ..
+
+echo "============" start server
+mix phx.server
