4268a17546bb6a8d5ceaf05a3493af62eea798e3
Bodo Tasche <bitboxer@users.noreply.github.com>
2020-03-13 15:01:14 +0100

    Change to webpack (#713)

---
+++ assets/webpack.config.js
@@ -0,0 +1,61 @@
+const path = require("path");
+const glob = require("glob");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
+
+module.exports = (env, options) => ({
+  devtool: "source-map",
+  optimization: {
+    minimizer: [
+      new TerserPlugin({ cache: true, parallel: true, sourceMap: false }),      new OptimizeCSSAssetsPlugin({})
+    ]
+  },
+  entry: {
+    "app": ["./js/app.js"].concat(glob.sync("./vendor/**/*.js")),
+    "backend": ["./js/backend.js"],
+    "recorder": ["./js/recorder.js"].concat(glob.sync("./vendor/**/*.js")),
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, "../priv/static/js")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.s?css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.vue$/,
+        use: 'vue-loader'
+      },
+      {
+        test: /\.(svg|woff|woff2|eot|ttf|json?)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "../fonts"
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new MiniCssExtractPlugin({ filename: "../css/[name].css" }),
+    new CopyWebpackPlugin([{ from: "static/", to: "../" }])
+  ]
+});
