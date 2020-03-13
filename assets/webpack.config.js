2b25c79d05cd14d3dee283027d70b94ba2d43a04
Bodo Tasche <bodo@wannawork.de>
2020-03-13 13:26:50 +0100

    adding webpack, step1

---
+++ assets/webpack.config.js
@@ -0,0 +1,54 @@
+const path = require("path");
+const glob = require("glob");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = (env, options) => ({
+  devtool: "source-map",
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({ cache: true, parallel: true, sourceMap: true }),
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  },
+  entry: {
+    "./js/app.js": ["./js/app.js"].concat(glob.sync("./vendor/**/*.js"))
+  },
+  output: {
+    filename: "app.js",
+    path: path.resolve(__dirname, "../priv/static/js")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.s?css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(svg|woff|woff2|eot|ttf|json?)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "../fonts"
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({ filename: "../css/app.css" }),
+    new CopyWebpackPlugin([{ from: "static/", to: "../" }])
+  ]
+});
--- config/dev.exs
+++ config/dev.exs
@@ -13,7 +13,13 @@ config :sign_dict, SignDictWeb.Endpoint,
   code_reloader: true,
   check_origin: false,
   watchers: [
-    node: ["node_modules/gulp/bin/gulp.js", "watch", cd: Path.expand("../assets", __DIR__)]
+    node: [
+      "node_modules/webpack/bin/webpack.js",
+      "--mode",
+      "development",
+      "--watch-stdin",
+      cd: Path.expand("../assets", __DIR__)
+    ]
   ]
 
 # Watch static and templates for browser reloading.
