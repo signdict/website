4613ba761afe87a820e2d997763847b45506ecfe
Bodo Tasche <bodo@wannawork.de>
2020-04-24 17:32:26 +0200

    [#144] Add search suggestions when typing
    closes #144

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,3 +27,10 @@ window.browser = detect();
 
 import Player from "./classes/player.js";
 new Player().init();
+
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+const application = Application.start()
+const context = require.context("./controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
