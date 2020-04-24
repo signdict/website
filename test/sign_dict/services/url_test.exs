e83b9b5dcd910c46ed8f91c33e89430ea214005b
Bodo Tasche <bodo@wannawork.de>
2020-04-24 16:28:33 +0200

    correct the url for other domains

--- test/sign_dict/services/url_test.exs
+++ test/sign_dict/services/url_test.exs
@@ -3,18 +3,21 @@ defmodule SignDict.Services.UrlTest do
 
   alias SignDict.Services.Url
 
-  describe "for_entry/1" do
+  describe "for_entry21" do
     test "adds correct url to entry" do
-      assert Url.for_entry(%SignDict.Entry{id: "1"}).url == "https://localhost/entry/1"
+      assert Url.for_entry(%SignDict.Entry{id: "1"}, "localhost").url ==
+               "https://localhost/entry/1"
     end
 
     test "adds correct url to videos" do
       entry = %SignDict.Entry{id: "1", videos: [%SignDict.Video{id: "2"}]}
-      assert List.first(Url.for_entry(entry).videos).url == "https://localhost/entry/1/video/2"
+
+      assert List.first(Url.for_entry(entry, "localhost").videos).url ==
+               "https://localhost/entry/1/video/2"
     end
 
     test "doest not crash on nil input" do
-      refute Url.for_entry(nil)
+      refute Url.for_entry(nil, "localhost")
     end
   end
 end
