f9898e504dedbbc7d231c6ba05956082603661a5
Bodo Tasche <bitboxer@users.noreply.github.com>
2020-04-26 13:54:57 +0200

    [#775] Improve Search Hints (#776)
    References #775

---
+++ priv/repo/migrations/20200426111221_add_trgm_index.exs
@@ -0,0 +1,8 @@
+defmodule SignDict.Repo.Migrations.AddTrgmIndex do
+  use Ecto.Migration
+
+  def change do
+    execute("CREATE EXTENSION pg_trgm;")
+    execute("CREATE INDEX trgm_entry_text ON entries USING GIN(text gin_trgm_ops);")
+  end
+end
