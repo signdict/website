0782673de4a880cd9eca70957d782c9775694c47
Bodo Tasche <bitboxer@users.noreply.github.com>
2020-06-05 16:14:17 +0200

    [#823] Elixir.MatchError in… (#852)
    …lib/sign_dict/worker/transcode_video.ex:16
    
    closes #823

--- lib/sign_dict/worker/transcode_video.ex
+++ lib/sign_dict/worker/transcode_video.ex
@@ -4,20 +4,38 @@ defmodule SignDict.Worker.TranscodeVideo do
   alias SignDict.Repo
   alias SignDict.Video
 
-  def perform(video_id, video_service \\ SignDict.Transcoder.JwPlayer, sleep_ms \\ 1000) do
+  # 10 minutes
+  @retry_upload 60 * 10
+
+  def perform(
+        video_id,
+        video_service \\ SignDict.Transcoder.JwPlayer,
+        sleep_ms \\ 1000,
+        exq \\ Exq
+      ) do
     Bugsnex.handle_errors %{video_id: video_id} do
-      upload_and_transcode(video_id, video_service)
+      upload_and_transcode(video_id, video_service, exq)
       # Rate limit the workers, sadly i didn't find a better way :(
       Process.sleep(sleep_ms)
     end
   end
 
-  defp upload_and_transcode(video_id, video_service) do
-    {:ok, video} =
+  defp upload_and_transcode(video_id, video_service, exq) do
+    result =
       Video
       |> Repo.get(video_id)
       |> video_service.upload_video()
 
-    Video.transcode(video)
+    case result do
+      {:ok, video} ->
+        Video.transcode(video)
+
+      _ ->
+        if Application.get_env(:sign_dict, :environment) != :dev do
+          exq.enqueue_in(Exq, "transcoder", @retry_upload, SignDict.Worker.TranscodeVideo, [
+            video_id
+          ])
+        end
+    end
   end
 end
--- test/sign_dict/worker/transcode_video_test.exs
+++ test/sign_dict/worker/transcode_video_test.exs
@@ -7,6 +7,12 @@ defmodule SignDict.Worker.TranscodeVideoTest do
   alias SignDict.Worker.TranscodeVideo
 
   describe "perform/2" do
+    defmodule ExqMockLater do
+      def enqueue_in(_arg1, _arg2, time, module, params) do
+        send(self(), {:enqueue_in, time, module, params})
+      end
+    end
+
     defmodule VideoServiceMock do
       @behaviour SignDict.Transcoder.API
       def upload_video(video) do
@@ -20,11 +26,32 @@ defmodule SignDict.Worker.TranscodeVideoTest do
       end
     end
 
+    defmodule VideoServiceFailedMock do
+      @behaviour SignDict.Transcoder.API
+      def upload_video(video) do
+        send(self(), {:transcode_video, video.id})
+        {:error, "this failed"}
+      end
+
+      def check_status(video) do
+        send(self(), {:check_status, video.id})
+        :transcoding
+      end
+    end
+
     test "it calls the video service to upload the video" do
       video_id = insert(:video, state: "uploaded").id
-      TranscodeVideo.perform(video_id, VideoServiceMock, 0)
+      TranscodeVideo.perform(video_id, VideoServiceMock, 0, ExqMockLater)
       assert_received {:transcode_video, ^video_id}
       assert Repo.get(Video, video_id).state == "transcoding"
+    end
+
+    test "it tries to upload again in 10 minutes if the upload failed" do
+      video_id = insert(:video, state: "uploaded").id
+      TranscodeVideo.perform(video_id, VideoServiceFailedMock, 0, ExqMockLater)
+      assert_received {:transcode_video, ^video_id}
+      assert Repo.get(Video, video_id).state == "uploaded"
+      assert_received {:enqueue_in, 600, SignDict.Worker.TranscodeVideo, [^video_id]}
     end
   end
 end
