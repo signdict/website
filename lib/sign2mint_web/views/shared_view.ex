e37cf0fb28c8409397e25111bda1ee2b4ef26349
Bodo Tasche <bitboxer@users.noreply.github.com>
2020-07-24 13:30:04 +0200

    add working filter (#926)

--- lib/sign2mint_web/views/shared_view.ex
+++ lib/sign2mint_web/views/shared_view.ex
@@ -2,4 +2,15 @@ defmodule Sign2MintWeb.SharedView do
   use Sign2MintWeb, :view
 
   import SignDict.OpengraphTags
+
+  def select_options(options, selected) do
+    Enum.map(options, fn option ->
+      {value, content} = option
+
+      content_tag(:option, content,
+        value: value,
+        selected: selected != nil && is_list(selected) && Enum.member?(selected, value)
+      )
+    end)
+  end
 end
