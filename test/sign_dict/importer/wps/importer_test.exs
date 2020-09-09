2136a5f9d249b6f7b34d7a6d6229873b87e7fc8d
Bodo Tasche <bitboxer@users.noreply.github.com>
2020-09-09 12:20:17 +0200

    check domain for entries (#997)

--- test/sign_dict/importer/wps/importer_test.exs
+++ test/sign_dict/importer/wps/importer_test.exs
@@ -22,7 +22,7 @@ defmodule SignDict.Importer.Wps.ImporterTest do
     end
 
     setup do
-      entry = insert(:entry, %{text: "Zug"})
+      entry = insert(:entry, %{text: "Zug", description: ""})
       user = insert(:user, %{name: "WPS"})
 
       Repo.insert!(%Video{
@@ -101,7 +101,7 @@ defmodule SignDict.Importer.Wps.ImporterTest do
              )
 
       assert video.entry.text == "Pi"
-      assert video.entry.description == ""
+      assert video.entry.description == "Fachgebärde aus dem Sign2MINT-Projekt"
       assert video.user.name == "WPS"
       assert List.first(video.entry.domains).domain == "test.local"
       assert Video.current_state(video) == :uploaded
@@ -227,6 +227,37 @@ defmodule SignDict.Importer.Wps.ImporterTest do
                |> Repo.aggregate(:count, :id)
 
       assert_received {:enqueue, SignDict.Worker.TranscodeVideo, [^video_id]}
+    end
+
+    test "it creates a new entry if the entry for this domain doesn't exist yet", %{entry: entry} do
+      start_time = Timex.parse!("2020-06-01T12:30:30+00:00", "%FT%T%:z", :strftime)
+
+      %ImporterConfig{}
+      |> ImporterConfig.changeset(%{
+        name: "WPS",
+        configuration: %{last_requested: start_time}
+      })
+      |> Repo.insert!()
+
+      videos = Importer.import_json(ExqMock)
+      video = List.first(videos)
+
+      assert video.entry_id != entry.id
+      assert video.entry.text == "Zug"
+    end
+
+    test "it does not crash if the video does not exist" do
+      start_time = Timex.parse!("2020-07-01T12:30:30+00:00", "%FT%T%:z", :strftime)
+
+      %ImporterConfig{}
+      |> ImporterConfig.changeset(%{
+        name: "WPS",
+        configuration: %{last_requested: start_time}
+      })
+      |> Repo.insert!()
+
+      videos = Importer.import_json(ExqMock)
+      assert videos == []
     end
 
     test "it deleted the file if it is marked for deletion" do
