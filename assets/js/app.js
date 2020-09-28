d652d33a57443d16cf3252e97554a2b5e5a7d081
Bodo Tasche <bodo@wannawork.de>
2020-09-28 14:16:27 +0200

    muting video and fixing de-init of player in turbolinks navigation

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,13 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import css from "../css/app.scss";
+import css from '../css/app.scss';
 
-import "phoenix_html";
-import "babel-polyfill";
+import 'phoenix_html';
+import 'babel-polyfill';
 
 if (window.Element && !Element.prototype.closest) {
-  Element.prototype.closest = function(s) {
+  Element.prototype.closest = function (s) {
     var matches = (this.document || this.ownerDocument).querySelectorAll(s),
       i,
       el = this;
@@ -19,35 +19,35 @@ if (window.Element && !Element.prototype.closest) {
   };
 }
 
-import Turbolinks from "turbolinks";
+import Turbolinks from 'turbolinks';
 Turbolinks.start();
 
-import {detect} from "detect-browser";
+import {detect} from 'detect-browser';
 window.browser = detect();
 
-import Player from "./classes/player.js";
-new Player().init();
+import Player from './classes/player.js';
+new Player();
 
-import { Application } from "stimulus"
-import { definitionsFromContext } from "stimulus/webpack-helpers"
+import {Application} from 'stimulus';
+import {definitionsFromContext} from 'stimulus/webpack-helpers';
 
-const application = Application.start()
-const context = require.context("./controllers", true, /\.js$/)
-application.load(definitionsFromContext(context))
+const application = Application.start();
+const context = require.context('./controllers', true, /\.js$/);
+application.load(definitionsFromContext(context));
 
 // Set autofocus to search bar on non-mobile devices
-window.onload = function() {
+window.onload = function () {
   var em = parseFloat(getComputedStyle(document.documentElement).fontSize);
 
   // Breakpoint is 48em, the same as in the CSS
   if (window.innerWidth > 48 * em) {
     // There is never more than one element with one of these classes, therefore this works
-    var searchbar_classes = ["so-searchbar--input--inner", "so-landing--search--input"];
+    var searchbar_classes = ['so-searchbar--input--inner', 'so-landing--search--input'];
     searchbar_classes.map((classname) => {
       var searchbars = document.getElementsByClassName(classname);
       if (searchbars[0]) {
         searchbars[0].focus();
       }
-    })
+    });
   }
 };
