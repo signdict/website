2c5569a530019d8bea9ba1cf0d3e6bc98aa23cd3
Bodo Tasche <bodo@wannawork.de>
2020-09-28 15:15:17 +0200

    refactored player.js

--- assets/js/classes/player.js
+++ assets/js/classes/player.js
@@ -1,15 +1,19 @@
 import Plyr from 'plyr';
 import translate from './translate';
 
+const INGORED_ELEMENTS = ['INPUT', 'A', 'BUTTON', 'TEXTAREA'];
+
 class Player {
   playerInstance = null;
   videoElement = null;
+  manualLoopCounter = 0;
+  playPressed = false;
 
   constructor() {
     document.addEventListener('turbolinks:load', () => {
       const videos = document.getElementsByTagName('video');
       if (videos.length > 0) {
-        this.playerInstance = addPlyr(videos[0]);
+        this.addPlyr(videos[0]);
         this.videoElement = videos[0];
         this.addKeyListener(videos[0]);
       }
@@ -42,17 +46,16 @@ class Player {
   };
 
   tabPlayrate = (e) => {
-    if (getKeyCode(e) == 9) {
+    if (this.getKeyCode(e) == 9) {
       let playrateButtons = document.getElementsByClassName('plyr_playrate');
       for (let element of playrateButtons) {
-        toggleClass(element, 'tab-focus', element == getFocusElement());
+        this.toggleClass(element, 'tab-focus', element == this.getFocusElement());
       }
     }
   };
-}
 
-function addPlyr(video) {
-  const controls = `
+  addPlyr = (video) => {
+    const controls = `
   <div class="plyr__controls">
     <button type="button" class="plyr__control" aria-label="Play, {title}" data-plyr="play">
         <svg class="icon--pressed" role="presentation"><use xlink:href="#plyr-pause"></use></svg>
@@ -80,117 +83,136 @@ function addPlyr(video) {
     </button>
   </div>`;
 
-  const player = new Plyr(video, {
-    controls,
-    muted: true,
-  });
-  let playrateButtons = document.getElementsByClassName('plyr_playrate');
-  for (let element of playrateButtons) {
-    element.onclick = togglePlayrateMenu;
-  }
-  return player;
-}
-
-function togglePlayrateMenu(event) {
-  let plyr = event.target.closest('.plyr');
-  let menu = plyr.getElementsByClassName('playrate-menu');
-  let button = event.target.closest('button');
-  if (menu.length == 0) {
-    showPlayrateMenu(plyr, button);
-  } else {
-    hidePlayrateMenu(menu[0], button);
-  }
-}
-
-function showPlayrateMenu(plyr, button) {
-  let menu = document.createElement('div');
-  menu.setAttribute('class', 'playrate-menu plyr__tooltip plyr__tooltip--visible');
-  menu.innerHTML =
-    "<ul class='playrate-menu--list'>" +
-    "<li><a class='playrate-menu--link' href='#' data-speed='0.25'>0.25x</a></li>" +
-    "<li><a class='playrate-menu--link' href='#' data-speed='0.5'>0.50x</a></li>" +
-    "<li><a class='playrate-menu--link' href='#' data-speed='0.75'>0.75x</a></li>" +
-    "<li><a class='playrate-menu--link' href='#' data-speed='1'>1.00x</a></li>" +
-    '</ul>';
-  plyr.appendChild(menu);
-  let plyrPosition = plyr.getBoundingClientRect().left;
-  let position = button.getBoundingClientRect().left - plyrPosition + button.clientWidth / 2;
-  menu.setAttribute('style', `left: ${position}px`);
-  toggleClass(button, 'playrate-menu--active', true);
-  linkPlayrateSpeedButton(plyr);
-  highlightCurrentPlayrate(plyr, menu);
-}
-
-function linkPlayrateSpeedButton(plyr) {
-  let playrateButtons = plyr.getElementsByClassName('playrate-menu--link');
-  for (let element of playrateButtons) {
-    element.onclick = function (event) {
-      updatePlaybackRate(plyr, event);
-    };
-    element.onkeyup = function (event) {
-      if (getKeyCode(event) == 13) {
-        updatePlaybackRate(plyr, event);
-      }
-    };
-  }
-}
-
-function updatePlaybackRate(plyr, event) {
-  let video = plyr.getElementsByTagName('video')[0];
-  let menu = plyr.getElementsByClassName('playrate-menu')[0];
-  let button = plyr.getElementsByClassName('plyr_playrate')[0];
-  let speed = parseFloat(event.target.getAttribute('data-speed'));
-  hidePlayrateMenu(menu, button);
-  video.playbackRate = speed;
-  event.preventDefault();
-}
-
-function highlightCurrentPlayrate(plyr, menu) {
-  let video = plyr.getElementsByTagName('video')[0];
-  let playrateButtons = plyr.getElementsByClassName('playrate-menu--link');
-  for (let element of playrateButtons) {
-    let speed = parseFloat(element.getAttribute('data-speed'));
-    if (video.playbackRate == speed) {
-      toggleClass(element, 'playrate-menu--link--selected', true);
-      setTimeout(function () {
-        element.focus();
-      }, 100);
+    this.playerInstance = new Plyr(video, {
+      controls,
+      muted: true,
+    });
+    let playrateButtons = document.getElementsByClassName('plyr_playrate');
+    for (let element of playrateButtons) {
+      element.onclick = this.togglePlayrateMenu;
     }
-  }
-}
 
-function hidePlayrateMenu(menu, button) {
-  menu.parentElement.removeChild(menu);
-  toggleClass(button, 'playrate-menu--active', false);
-}
+    if (video.getAttribute('data-plyr-manual-loop') == 'true') {
+      this.initManualLoop();
+    }
+  };
 
-const INGORED_ELEMENTS = ['INPUT', 'A', 'BUTTON', 'TEXTAREA'];
-
-function toggleClass(element, className, state) {
-  if (element) {
-    if (element.classList) {
-      element.classList[state ? 'add' : 'remove'](className);
+  togglePlayrateMenu = (event) => {
+    let plyr = event.target.closest('.plyr');
+    let menu = plyr.getElementsByClassName('playrate-menu');
+    let button = event.target.closest('button');
+    if (menu.length == 0) {
+      this.showPlayrateMenu(plyr, button);
     } else {
-      var name = (' ' + element.className + ' ').replace(/\s+/g, ' ').replace(' ' + className + ' ', '');
-      element.className = name + (state ? ' ' + className : '');
+      this.hidePlayrateMenu(menu[0], button);
     }
-  }
-}
+  };
 
-function getKeyCode(event) {
-  return event.keyCode ? event.keyCode : event.which;
-}
+  showPlayrateMenu = (plyr, button) => {
+    let menu = document.createElement('div');
+    menu.setAttribute('class', 'playrate-menu plyr__tooltip plyr__tooltip--visible');
+    menu.innerHTML =
+      "<ul class='playrate-menu--list'>" +
+      "<li><a class='playrate-menu--link' href='#' data-speed='0.25'>0.25x</a></li>" +
+      "<li><a class='playrate-menu--link' href='#' data-speed='0.5'>0.50x</a></li>" +
+      "<li><a class='playrate-menu--link' href='#' data-speed='0.75'>0.75x</a></li>" +
+      "<li><a class='playrate-menu--link' href='#' data-speed='1'>1.00x</a></li>" +
+      '</ul>';
+    plyr.appendChild(menu);
+    let plyrPosition = plyr.getBoundingClientRect().left;
+    let position = button.getBoundingClientRect().left - plyrPosition + button.clientWidth / 2;
+    menu.setAttribute('style', `left: ${position}px`);
+    this.toggleClass(button, 'playrate-menu--active', true);
+    this.linkPlayrateSpeedButton(plyr);
+    this.highlightCurrentPlayrate(plyr, menu);
+  };
 
-function getFocusElement() {
-  let focused = document.activeElement;
+  linkPlayrateSpeedButton = (plyr) => {
+    let playrateButtons = plyr.getElementsByClassName('playrate-menu--link');
+    for (let element of playrateButtons) {
+      element.onclick = (event) => {
+        this.updatePlaybackRate(plyr, event);
+      };
+      element.onkeyup = (event) => {
+        if (this.getKeyCode(event) == 13) {
+          this.updatePlaybackRate(plyr, event);
+        }
+      };
+    }
+  };
 
-  if (!focused || focused === document.body) {
-    focused = null;
-  } else {
-    focused = document.querySelector(':focus');
-  }
+  updatePlaybackRate = (plyr, event) => {
+    let video = plyr.getElementsByTagName('video')[0];
+    let menu = plyr.getElementsByClassName('playrate-menu')[0];
+    let button = plyr.getElementsByClassName('plyr_playrate')[0];
+    let speed = parseFloat(event.target.getAttribute('data-speed'));
+    this.hidePlayrateMenu(menu, button);
+    video.playbackRate = speed;
+    event.preventDefault();
+  };
 
-  return focused;
+  highlightCurrentPlayrate = (plyr, menu) => {
+    let video = plyr.getElementsByTagName('video')[0];
+    let playrateButtons = plyr.getElementsByClassName('playrate-menu--link');
+    for (let element of playrateButtons) {
+      let speed = parseFloat(element.getAttribute('data-speed'));
+      if (video.playbackRate == speed) {
+        this.toggleClass(element, 'playrate-menu--link--selected', true);
+        setTimeout(() => {
+          element.focus();
+        }, 100);
+      }
+    }
+  };
+
+  hidePlayrateMenu = (menu, button) => {
+    menu.parentElement.removeChild(menu);
+    this.toggleClass(button, 'playrate-menu--active', false);
+  };
+
+  toggleClass = (element, className, state) => {
+    if (element) {
+      if (element.classList) {
+        element.classList[state ? 'add' : 'remove'](className);
+      } else {
+        var name = (' ' + element.className + ' ').replace(/\s+/g, ' ').replace(' ' + className + ' ', '');
+        element.className = name + (state ? ' ' + className : '');
+      }
+    }
+  };
+
+  getKeyCode = (event) => {
+    return event.keyCode ? event.keyCode : event.which;
+  };
+
+  getFocusElement = () => {
+    let focused = document.activeElement;
+
+    if (!focused || focused === document.body) {
+      focused = null;
+    } else {
+      focused = document.querySelector(':focus');
+    }
+
+    return focused;
+  };
+
+  initManualLoop = () => {
+    this.manualLoopCounter = 0;
+    this.playerInstance.on('ended', () => {
+      this.manualLoopCounter += 1;
+      if (this.manualLoopCounter < 3) {
+        this.playPressed = false;
+        this.playerInstance.play();
+      }
+    });
+    this.playerInstance.on('play', () => {
+      if (this.playPressed) {
+        this.playerInstance.loop = true;
+      }
+      this.playPressed = true;
+    });
+  };
 }
 
 export default Player;
